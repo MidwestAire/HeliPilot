b6390fe3227a1d8b67130d53ff14168528478496
ChristopherOlson <chriskristin.olson@gmail.com>
2019-03-18 18:37:42 -0500

    HeliPilot 19.08.05 Pre-Release 05 August, 2019

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,12 +6,12 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V3.6.12"
+#define THISFIRMWARE "HeliPilot V19.08.05"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 3,6,12,FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FIRMWARE_VERSION 19,8,5,FIRMWARE_VERSION_TYPE_OFFICIAL
 
-#define FW_MAJOR 3
-#define FW_MINOR 6
-#define FW_PATCH 12
+#define FW_MAJOR 19
+#define FW_MINOR 8
+#define FW_PATCH 5
 #define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
