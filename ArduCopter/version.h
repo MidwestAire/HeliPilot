d30d7cb3d969ae15fd7a4dd2b49c8eb9075285ac
ChristopherOlson <ChrisKristin.Olson@gmail.com>
2019-11-21 18:18:40 -0600

    HeliPilot:master to v20.01.01-dev

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,12 +6,12 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "HeliPilot V19.10.01"
+#define THISFIRMWARE "HeliPilot v20.01.01-dev"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 19,10,01,FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FIRMWARE_VERSION 20,01,01,FIRMWARE_VERSION_TYPE_DEV
 
-#define FW_MAJOR 19
-#define FW_MINOR 10
+#define FW_MAJOR 20
+#define FW_MINOR 01
 #define FW_PATCH 01
 #define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
