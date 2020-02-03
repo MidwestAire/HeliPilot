ab53a9bbd38b570dd4da04995991ccb72bd037db
ChristopherOlson <ChrisKristin.Olson@gmail.com>
2020-02-03 11:54:37 -0600

    update git modules

--- Tools/scripts/install-prereqs-ubuntu.sh
+++ Tools/scripts/install-prereqs-ubuntu.sh
@@ -5,13 +5,13 @@ set -x
 
 OPT="/opt"
 BASE_PKGS="build-essential ccache g++ gawk git make wget"
-PYTHON_PKGS="future lxml pymavlink MAVProxy"
+PYTHON_PKGS="future lxml pymavlink MAVProxy pexpect"
 PX4_PKGS="python-argparse openocd flex bison libncurses5-dev \
           autoconf texinfo libftdi-dev zlib1g-dev \
           zip genromfs python-empy cmake cmake-data"
 ARM_LINUX_PKGS="g++-arm-linux-gnueabihf pkg-config-arm-linux-gnueabihf"
 # python-wxgtk packages are added to SITL_PKGS below
-SITL_PKGS="libtool libxml2-dev libxslt1-dev python-dev python-pip python-setuptools python-matplotlib python-serial python-scipy python-opencv python-numpy python-pyparsing xterm"
+SITL_PKGS="libtool libxml2-dev libxslt1-dev python-dev python-pip python-setuptools python-matplotlib python-serial python-scipy python-opencv python-numpy python-pyparsing xterm lcov gcovr"
 ASSUME_YES=false
 QUIET=false
 
@@ -24,12 +24,12 @@ fi
 
 # GNU Tools for ARM Embedded Processors
 # (see https://launchpad.net/gcc-arm-embedded/)
-ARM_ROOT="gcc-arm-none-eabi-4_9-2015q3"
-ARM_TARBALL="$ARM_ROOT-20150921-linux.tar.bz2"
-ARM_TARBALL_URL="http://firmware.ardupilot.org/Tools/PX4-tools/$ARM_TARBALL"
+ARM_ROOT="gcc-arm-none-eabi-6-2017-q2-update"
+ARM_TARBALL="$ARM_ROOT-linux.tar.bz2"
+ARM_TARBALL_URL="https://github.com/MidwestAire/HeliPilot/releases/download/v20.02.01/$ARM_TARBALL"
 
-# Ardupilot Tools
-ARDUPILOT_TOOLS="Tools/autotest"
+# HeliPilot Tools
+HELIPILOT_TOOLS="Tools/autotest"
 
 function maybe_prompt_user() {
     if $ASSUME_YES; then
@@ -66,6 +66,9 @@ if $QUIET; then
     APT_GET="$APT_GET -qq"
 fi
 
+if ! dpkg-query -l "lsb-release"; then
+    $APT_GET install lsb-release
+fi
 # possibly grab a newer cmake for older ubuntu releases
 read -r UBUNTU_CODENAME <<<$(lsb_release -c -s)
 if [ "$UBUNTU_CODENAME" = "precise" ]; then
@@ -76,7 +79,9 @@ fi
 
 sudo usermod -a -G dialout $USER
 
-$APT_GET remove modemmanager
+if dpkg-query -l "modemmanager"; then
+    $APT_GET remove modemmanager
+fi
 $APT_GET update
 
 if apt-cache search python-wxgtk3.0 | grep wx; then
@@ -92,7 +97,7 @@ if [ -n "$RP" ]; then
 fi
 
 $APT_GET install $BASE_PKGS $SITL_PKGS $PX4_PKGS $ARM_LINUX_PKGS
-sudo pip2 -q install -U $PYTHON_PKGS
+pip2 -q install --user -U $PYTHON_PKGS
 
 if [ ! -d $OPT/$ARM_ROOT ]; then
     (
@@ -104,11 +109,11 @@ if [ ! -d $OPT/$ARM_ROOT ]; then
 fi
 
 SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
-ARDUPILOT_ROOT=$(realpath "$SCRIPT_DIR/../../")
+HELIPILOT_ROOT=$(realpath "$SCRIPT_DIR/../../")
 
 exportline="export PATH=$OPT/$ARM_ROOT/bin:\$PATH";
 grep -Fxq "$exportline" ~/.profile 2>/dev/null || {
-    if maybe_prompt_user "Add $OPT/$ARM_ROOT/bin to your PATH [Y/n]?" ; then
+    if maybe_prompt_user "Add $OPT/$ARM_ROOT/bin to your PATH [N/y]?" ; then
         echo $exportline >> ~/.profile
         eval $exportline
     else
@@ -116,21 +121,20 @@ grep -Fxq "$exportline" ~/.profile 2>/dev/null || {
     fi
 }
 
-exportline2="export PATH=$ARDUPILOT_ROOT/$ARDUPILOT_TOOLS:\$PATH";
+exportline2="export PATH=$HELIPILOT_ROOT/$HELIPILOT_TOOLS:\$PATH";
 grep -Fxq "$exportline2" ~/.profile 2>/dev/null || {
-    if maybe_prompt_user "Add $ARDUPILOT_ROOT/$ARDUPILOT_TOOLS to your PATH [Y/n]?" ; then
+    if maybe_prompt_user "Add $HELIPILOT_ROOT/$HELIPILOT_TOOLS to your PATH [N/y]?" ; then
         echo $exportline2 >> ~/.profile
         eval $exportline2
     else
-        echo "Skipping adding $ARDUPILOT_ROOT/$ARDUPILOT_TOOLS to PATH."
+        echo "Skipping adding $HELIPILOT_ROOT/$HELIPILOT_TOOLS to PATH."
     fi
 }
 
 apt-cache search arm-none-eabi
 
 (
- cd $ARDUPILOT_ROOT
- git submodule init
- git submodule update
+ cd $HELIPILOT_ROOT
+ git submodule update --init --recursive
 )
 echo "---------- $0 end ----------"
