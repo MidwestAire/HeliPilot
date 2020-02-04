90b4599cc2525592806599108ae6a7f622cc118b
ChristopherOlson <ChrisKristin.Olson@gmail.com>
2020-02-03 18:58:13 -0600

    Tools: add new scripted build tools to v19

--- Tools/scripts/install-prereqs-mac.sh
+++ Tools/scripts/install-prereqs-mac.sh
@@ -11,7 +11,7 @@ fi
 #install xconde dependencies
 xcode-select --install
 
-brew tap PX4/homebrew-px4
+brew tap ardupilot/homebrew-px4
 brew update
 brew install genromfs
 brew install gcc-arm-none-eabi
@@ -26,7 +26,7 @@ else
     echo "pip installed"
 fi
 
-sudo pip2 install pyserial future catkin_pkg empy
+pip2 install --user pyserial future empy mavproxy pexpect
 
 SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 ARDUPILOT_ROOT=$(realpath "$SCRIPT_DIR/../../")
@@ -34,7 +34,7 @@ ARDUPILOT_TOOLS="Tools/autotest"
 
 exportline="export PATH=$ARDUPILOT_ROOT/$ARDUPILOT_TOOLS:\$PATH";
 grep -Fxq "$exportline" ~/.profile 2>/dev/null || {
-    read -p "Add $ARDUPILOT_ROOT/$ARDUPILOT_TOOLS to your PATH [Y/n]?" -n 1 -r
+    read -p "Add $ARDUPILOT_ROOT/$ARDUPILOT_TOOLS to your PATH [N/y]?" -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]] ; then
         echo $exportline >> ~/.profile
         eval $exportline
